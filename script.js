1a982debabf21ec7afcf02f0e7151047b13d786a
tylerk323 <tylermklappauf@lewisu.edu>
2026-05-12 00:09:01 -0500

    Implement save and load homework functionality
    Add functions to save and load homework items from localStorage.

--- script.js
+++ script.js
@@ -19,8 +19,48 @@ li.onclick = function () {
   li.style.textDecoration = "line-through";
 };
   list.appendChild(li);
+
+  saveHomework();
 }
 
 function toggleMode() {
   document.body.classList.toggle("dark-mode");
 }
+
+function saveHomework() {
+  let listItems = 
+    document.querySelectorAll("homeworkList li");
+  let homeworkArray = [];
+  listItems.forEach(function(item) {
+    homeworkArray.push({
+      text: item.innerGTML,
+      classes: item.className
+    });
+  }),
+
+    localStorage.setItem(
+      "homework",
+      JSON.stringify(homeworkArray)
+      );
+}
+
+fucntion loadHomework() {
+  let savedHomework =
+    JSON.parse(localStorage.getItem("homework"));
+
+  if (savedHomework) {
+    savedHomework.forEach(function(item) {
+      let li =
+        document.createElement("li");
+      li.innerHTML = item.text;
+      li.className = item.classes;
+      li.onclick = function () {
+        li.classList.toggle("completed");
+        savedHomework();
+      };
+      document.getElementById("homeworkList")
+      .appendChild(li);
+    });
+  }
+}
+window.onload = loadHomework;
